e57f0891db4fe8345f49a0045b6c07d4d5adb2aa
Liam McGovern <liammcgovern@gmail.com>
2013-11-28 18:58:28 -0500

    Fixed algorithm, makes sense now, only requires minor edits to be put into function.

--- Assign4/src/Assign4.java
+++ Assign4/src/Assign4.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Assign4 {
 	private int iArrLength;
 	static Scanner input = new Scanner(System.in);
+	private int iEnteredNumbers[];
 	private int iUniqueNumbers[];
-	private boolean bNotUnique[];
+	private int iUniqueIndexCount = 0;
 
 	Assign4() {
 		iArrLength = 0;
@@ -25,8 +26,9 @@ public class Assign4 {
 				System.out.println("Please enter a valid integer");
 			}
 		}
+		iEnteredNumbers = new int[iArrLength];
 		iUniqueNumbers = new int[iArrLength];
-		bNotUnique = new boolean[iArrLength];
+
 	}
 
 	private void getArrayData() {
@@ -36,8 +38,8 @@ public class Assign4 {
 
 			while (true) {
 				try {
-					iUniqueNumbers[i] = Integer.parseInt(input.next()); //don't allow user to input anything but a a Int
-					bNotUnique[i]=false;
+					iEnteredNumbers[i] = Integer.parseInt(input.next()); //don't allow user to input anything but a a Int
+//					bIsUnique[i]=false;
 					break;
 				} catch (NumberFormatException e) {
 					System.out.println("Please enter a valid integer");
@@ -50,36 +52,44 @@ public class Assign4 {
 	/*
 	 * New Idea, make a second list, and create a loop that enters a number, 
 	 * if it's not already in the list, display that list
-	 * this eliminates the issues that result of setting non uniques to 1.
+	 * this eliminates the issues that result of setting non unique numbers to 1.
 	 */
 	private void findUniqueNumbers() {
 		if (iArrLength > 1){ //no sense in checking for unique numbers if there is only 1,
-			for (int i = 1; i < iArrLength; i++) { //starting from the last position in the index, subtract one until you are at the first.
-				for(int j = 0; j< iArrLength; j++){//starting at the first position in the index, increasing until the limit
-					if (iUniqueNumbers[i] == iUniqueNumbers[j] && i !=j ){
-						bNotUnique[i]= true;
-						//break;
+			int uniqueIndex=0;
+			for (int i = 0; i < iArrLength; i++) { //starting from the last position in the index, subtract one until you are at the first.
+				
+				for (int j = 0; j <iArrLength; j++){
+					if (j==i){
+						continue;
+					}
+					if (iEnteredNumbers[i]==iEnteredNumbers[j]){
+						break;
+					}
+					if (j == iArrLength-1){
+						iUniqueNumbers[uniqueIndex] = iEnteredNumbers[i];
+						iUniqueIndexCount++;
 					}
 				}
 			}
 			
 		} else {
-			System.out.print("The unique number you entered is:\t");
-			System.out.println(iUniqueNumbers[0]);
+			System.out.print("The single (unique) number you entered is:\t");
+			System.out.println(iEnteredNumbers[0]);
 		}
 	}
 	private void test() {
 		for (int i = 0; i < iArrLength; i++) {
-			System.out.println("The value of the array is " + iUniqueNumbers[i]);
+			System.out.println("The value of the array is " + iEnteredNumbers[i]);
 		}
 	}
 	private void displayUniqueNumbers(){
 		System.out.print("The unique numbers you entered are:\t");
-		for (int i = 0; i < iArrLength; i++) {
-			if(bNotUnique[i] == false){
-				System.out.print(iUniqueNumbers[i]);
-			}
-		}
+//		for (int i = 0; i < iArrLength; i++) {
+//			if(bIsUnique[i] == true){
+//				System.out.print(iEnteredNumbers[i]);
+//			}
+//		}
 	}
 
 	public static void main(String[] args) {
