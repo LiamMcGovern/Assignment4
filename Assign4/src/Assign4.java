b1a6ae4ac269db9242f5fb6d03d78b09add295db
Liam McGovern <liammcgovern@gmail.com>
2013-11-29 20:56:19 -0500

    jive turkey completion

--- Assign4/src/Assign4.java
+++ Assign4/src/Assign4.java
@@ -5,19 +5,19 @@ public class Assign4 {
 	static Scanner input = new Scanner(System.in);
 	private int iEnteredNumbers[];
 	private int iUniqueNumbers[];
-	private int iUniqueIndexCount = 0;
+	public int iUniqueIndexCount = 0;
 
 	Assign4() {
 		iArrLength = 0;
+		iUniqueIndexCount=0;
 	}
 
 	private void getArrayLength() {
-		System.out.println("Please enter the quanity of unique numbers you will enter");
-
+		System.out.println("Please enter the quanity of numbers you will enter");
 		while (true) {
 			try {
-				iArrLength = Integer.parseInt(input.next()); //don't allow user to input anything but a a Int
-				if (iArrLength > 0){
+				iArrLength = Integer.parseInt(input.next()); 
+				if (iArrLength > 0) {
 					break;
 				} else {
 					System.out.println("Enter a number greater than 0");
@@ -38,58 +38,54 @@ public class Assign4 {
 
 			while (true) {
 				try {
-					iEnteredNumbers[i] = Integer.parseInt(input.next()); //don't allow user to input anything but a a Int
-//					bIsUnique[i]=false;
+					iEnteredNumbers[i] = Integer.parseInt(input.next());  
+					iUniqueNumbers[i] = 0;
 					break;
 				} catch (NumberFormatException e) {
 					System.out.println("Please enter a valid integer");
 				}
 			}
 		}
-}
-	//}
-//
-	/*
-	 * New Idea, make a second list, and create a loop that enters a number, 
-	 * if it's not already in the list, display that list
-	 * this eliminates the issues that result of setting non unique numbers to 1.
-	 */
+	}
+
 	private void findUniqueNumbers() {
-		if (iArrLength > 1){ //no sense in checking for unique numbers if there is only 1,
-			int uniqueIndex=0;
-			for (int i = 0; i < iArrLength; i++) { //starting from the last position in the index, subtract one until you are at the first.
-				
-				for (int j = 0; j <iArrLength; j++){
-					if (j==i){
-						continue;
-					}
-					if (iEnteredNumbers[i]==iEnteredNumbers[j]){
+		if (iArrLength > 1) { 
+			//int iUniqueIndexCount = 0;
+			for (int i = 0; i < iArrLength; i++) {
+				for (int j = 0; j < iArrLength; j++) {
+					//if (i == j) {
+					//	continue;
+					//}
+					if (iEnteredNumbers[i] == iUniqueNumbers[j] ) {
+						System.out.println("going to break now" + i + j);
 						break;
 					}
-					if (j == iArrLength-1){
-						iUniqueNumbers[uniqueIndex] = iEnteredNumbers[i];
+					
+					if (j == iArrLength - 1) {
+						System.out.printf("going to add it to array now i is at %d, j is at %d and the number to enter is %d%n", i, j, iEnteredNumbers[i] );
+						iUniqueNumbers[iUniqueIndexCount] = iEnteredNumbers[i];
 						iUniqueIndexCount++;
 					}
 				}
 			}
-			
+
 		} else {
 			System.out.print("The single (unique) number you entered is:\t");
 			System.out.println(iEnteredNumbers[0]);
 		}
 	}
+
 	private void test() {
 		for (int i = 0; i < iArrLength; i++) {
-			System.out.println("The value of the array is " + iEnteredNumbers[i]);
+			System.out.println("The value of the array is "	+ iEnteredNumbers[i]);
 		}
 	}
-	private void displayUniqueNumbers(){
+
+	private void displayUniqueNumbers() {
 		System.out.print("The unique numbers you entered are:\t");
-//		for (int i = 0; i < iArrLength; i++) {
-//			if(bIsUnique[i] == true){
-//				System.out.print(iEnteredNumbers[i]);
-//			}
-//		}
+		for (int i = 0; i < iUniqueIndexCount; i++) {
+			System.out.print(iUniqueNumbers[i]);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -102,7 +98,6 @@ public class Assign4 {
 		ign4.findUniqueNumbers();
 		ign4.displayUniqueNumbers();
 
-
-	} 
+	}
 
 }
