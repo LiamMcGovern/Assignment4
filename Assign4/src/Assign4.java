3bda41cbbcc64f1980531bc75fb2e068dad30b0d
Liam McGovern <liammcgovern@gmail.com>
2013-12-06 11:03:04 -0500

    Added comments

--- Assign4/src/Assign4.java
+++ Assign4/src/Assign4.java
@@ -1,8 +1,8 @@
 public class Assign4 {
 	public static void main(String[] args) {
 		UniqueNumbers un1 = new UniqueNumbers();
-		un1.getAmountOfNumbers();
-		un1.getNumbersEntered();
-		un1.findUniqueNumbers();
+		un1.getAmountOfNumbers(); //get quantity of numbers to be entered from user.
+		un1.getNumbersEntered(); // get those numbers.
+		un1.findUniqueNumbers(); //find the unique numbers and display them.
 	}
 }
--- Assign4/src/UniqueNumbers.java
+++ Assign4/src/UniqueNumbers.java
@@ -1,41 +1,36 @@
 import java.util.Scanner;
 
 public class UniqueNumbers {
-	Scanner input = new Scanner(System.in);
-	private int iArrLength;
-	private int iEnteredNumbers[];
-	private int iUniqueNumbers[];
-	private int iUniqueIndexCount;
-	private boolean bUniqueZero;
-
-	UniqueNumbers() {
+	Scanner input = new Scanner(System.in); //Allow input
+	private int iArrLength; //Used to defines the length of the Arrays
+	private int iEnteredNumbers[]; //Array to hold the values of the Entered Numbers
+	private int iUniqueNumbers[]; //Array to hold the values of the Unique Numbers
+	
+	UniqueNumbers() { //Default constructor
 		iArrLength = 0;
-		iUniqueIndexCount = 0;
-		bUniqueZero = false;
 	}
 
-	public void getAmountOfNumbers() {
+	public void getAmountOfNumbers() { //Get the quantity of numbers to be entered.
 		System.out.println("Please enter the quanity of numbers you will enter");
 		// Only allow Integer input
 		while (true) {
 			try {
 				iArrLength = Integer.parseInt(input.next());
-				if (iArrLength > 0) {
+				if (iArrLength > 0) { 
 					break;
-				} else {
+				} else { //Prevents numbers entered being less than or equal to 0.
 					System.out.println("Enter a number greater than 0");
 				}
-			} catch (NumberFormatException e) {
+			} catch (NumberFormatException e) { //Prevents non integer input.
 				System.out.println("Please enter a valid integer");
 			}
 		}
-		iEnteredNumbers = new int[iArrLength];
+		// Define scope of arrays
+		iEnteredNumbers = new int[iArrLength]; 
 		iUniqueNumbers = new int[iArrLength];
-
 	}
 
-	public void getNumbersEntered() {
-
+	public void getNumbersEntered() { //Get each number to be put in iEnteredNumbers at position i.
 		System.out.println("Please enter " + iArrLength + " number(s):");
 		for (int i = 0; i < iArrLength; i++) {
 			// Only allow Integer input
@@ -44,46 +39,45 @@ public class UniqueNumbers {
 					iEnteredNumbers[i] = Integer.parseInt(input.next());
 					iUniqueNumbers[i] = 0;
 					break;
-				} catch (NumberFormatException e) {
+				} catch (NumberFormatException e) { //Prevents non integer input.
 					System.out.println("Please enter a valid integer");
-				}
-			}
-		}
-		input.close();
-	}
+				}//end catch
+			}//end while
+		}//end for
+		input.close(); //Scanner is no longer used at this point, therefore it can be safely closed.
+	} //end gNE
 
-	public void findUniqueNumbers() {
-		// Find the numbers
-		if (iArrLength > 1) {
-			for (int i = 0; i < iArrLength; i++) {
-				for (int j = 0; j < iArrLength; j++) {
-					if (iEnteredNumbers[i] == iUniqueNumbers[j]) {
-						if (iUniqueNumbers[j] == 0 && bUniqueZero == false) {
+	public void findUniqueNumbers() {  //Find and display the Unique Numbers
+		boolean boUniqueZero = false; //Used to check if a zero has been entered into the unique numbers.
+		int iUniqueIndexCount = 0; //Used to count and later display the number of unique numbers.
+		if (iArrLength > 1) { //If there is only one number entered, no sense in checking for unique numbers.
+			for (int i = 0; i < iArrLength; i++) { //i in this loop is for the position of iEnteredNumbers, that will be check against j in iUniqueNumbers
+				for (int j = 0; j < iArrLength; j++) { //j in this loop is for the position in iUniqueNumbers
+					if (iEnteredNumbers[i] == iUniqueNumbers[j]) { //If the value of the position we are checking is equal to any value iUnique then....
+						if (iUniqueNumbers[j] == 0 && boUniqueZero == false) { 
 							iUniqueNumbers[iUniqueIndexCount] = 0;
 							iUniqueIndexCount++;
-							bUniqueZero = true;
-							break;
-						}
+							boUniqueZero = true;
+							break; 
+						}//end if
 						break;
 
-					}
-
+					} // end if
+					//If we reach reach the scope of the array without finding a match, add it to iUniqueNumbes.
 					if (j == iArrLength - 1) {
 						iUniqueNumbers[iUniqueIndexCount] = iEnteredNumbers[i];
 						iUniqueIndexCount++;
-					}
-				}
-			}
+					} // end if
+				}// end for j
+			}// end for i
 			//Display the unique numbers
 			System.out.print("The unique numbers you entered are:\t");
 			for (int i = 0; i < iUniqueIndexCount; i++) {
 				System.out.print(iUniqueNumbers[i] + " ");
-			}
-		} else { // If the user enters a single number, no sense in displaying
-					// the numbers twice
+			}// end for i
+		} else { // If the user enters a single number, no sense in displaying the numbers twice
 			System.out.print("The single (unique) number you entered is:\t");
 			System.out.println(iEnteredNumbers[0]);
-		}
-	}
-
-}
+		} //end else
+	}//end fUN
+}//end UN class
